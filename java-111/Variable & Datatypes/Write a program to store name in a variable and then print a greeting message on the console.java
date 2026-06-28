0410a40b6294ec6fe99f1661febe381d7b3d9558
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2026-06-28 10:30:48 +0530

    Greet on Console
    Implemented a Java program to store a name and print a greeting message.

---
+++ java-111/Variable & Datatypes/Write a program to store name in a variable and then print a greeting message on the console.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String args[]) {
+        // Write a program to store name in a variable and
+        // then print a greeting message on the console.
+
+        // Eg: name = John
+        // Console output: Hello John.
+        String name = "greeting msg";
+        System.out.println("Hello " + name);
+    }
+}
+
+
