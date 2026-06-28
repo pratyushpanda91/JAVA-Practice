272c26e218af7944ed4895f553a68d1cd32787c9
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2026-06-28 10:42:09 +0530

    Add Java program for rectangle area and perimeter
    Implemented a Java program to calculate the area and perimeter of a rectangle.

---
+++ java-1/Variable & Datatypes/Area and perimeter of a rectangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String args[]) {
+        // Write a java program to find area and perimeter of rectangle.
+        int l = 5;
+        int b = 6;
+        int area = l*b ;
+        int perimeter = 2*(l+b);
+        System.out.println(area);
+        System.out.println(perimeter);
+        
+    }
+}
+
+
