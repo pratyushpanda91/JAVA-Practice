0eb50482f656482a866bf1a97792bf65f1c05ccf
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2026-06-30 22:06:29 +0530

    Add Java program to concatenate characters
    Implemented a Java program to concatenate characters 'J', 'A', 'V', 'A' and print the result.

---
+++ java-1/Variable & Datatypes/Concatenate characters3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String args[]) {
+        //Write code to concatenate characters ‘J’,’A’,’V’,’A’ to a string and print it on the console.
+        // Store characters 'J','A','V','A' in variables
+        // Concatenate all the characters and print in on console
+        char s1 = 'J';
+        char s2 = 'A';
+        char s3 = 'V';
+        char s4 = 'A';
+        System.out.println(""+s1+s2+s3+s4);
+    }
+}
+
+
