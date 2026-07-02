5340b65a10562073993af253d75558ec107f17cd
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2026-07-02 21:46:09 +0530

    Add program to convert years to days
    Implement a program to convert years into days.

---
+++ java-1/Variable & Datatypes/Convert years into days4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String args[]) {
+        // Write your code to convert Years into Days (Consider 1 year = 365 Days)
+        // Years = 2 -> Days = 730 Days
+        // Print number of Days on console
+        int year = 2;
+        int days = 365 * year;
+        System.out.println(days);
+    }
+}
+
+
