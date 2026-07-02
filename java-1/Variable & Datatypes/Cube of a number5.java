7d91c2b4680c728bd0e4e5f6762162e96bdf51ee
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2026-07-02 21:48:42 +0530

    Add Java program to calculate cube of a number

---
+++ java-1/Variable & Datatypes/Cube of a number5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String args[]) {
+        // Write code to print cube of a number and print it on console
+        // Eg: cube of 4 -> 4 * 4 * 4 = 64
+        int num = 4;
+        int cube = num * num * num;
+        System.out.println(cube);
+    }
+}
+
+
